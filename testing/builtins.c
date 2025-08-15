37d030cd64e5ecb071d3aa0fde8a1b79ac615a42
cipher <ciphershadow1@gmail.com>
2025-08-16 08:00:50 +1000

    added notes to builtins.c

--- testing/builtins.c
+++ testing/builtins.c
@@ -9,11 +9,13 @@
 
 int handle_builtin(char *input)
 {
+	/* performs strcmp and if both are 0 then shell will exit */
 	if (_strcmp(input, "exit") == 0)
 	{
 		exit(0);
 	}
-	else if (_strcmp(input, "env") == 0)
+	/* performs strcmp and if both equal 0 then print_env() is called */
+	else if (_strcmp(input, "env") == 0) 
 	{
 		print_env();
 		return (1);
@@ -32,8 +34,10 @@ void print_env(void)
 {
 	int i;
 
+	/* uses global environ and loops until NULL */
 	for (i = 0; environ[i] != NULL; i++)
 	{
+		/* prints out environment variables followed by newline */
 		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 	}
