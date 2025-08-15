a27ae613002716ff24f090113e87aa10f2d0811b
cipher <ciphershadow1@gmail.com>
2025-08-15 03:47:21 +0000

    addressed declaratio typo and added print_banner

--- testing/main.c
+++ testing/main.c
@@ -1,6 +1,39 @@
 #include "shell.h"
 
 /**
+ * print_banner - function prints ascii banner when program runs
+ *
+ * Return: void
+ */
+
+void print_banner(void)
+{
+	printf("\033[1;32m");
+
+	printf(" ___  ___  ________  ___  ___     \n");
+	usleep(50000);
+	printf("|\\  \\|\\  \\|\\   ____\\|\\  \\|\\  \\    \n");
+	usleep(50000);
+	printf("\\ \\  \\\\\\  \\ \\  \\___|\\ \\  \\\\\\  \\   \n");
+	usleep(50000);
+	printf(" \\ \\   __  \\ \\_____  \\ \\   __  \\  \n");
+	usleep(50000);
+	printf("  \\ \\  \\ \\  \\|____|\\  \\ \\  \\ \\  \\ \n");
+	usleep(50000);
+	printf("   \\ \\__\\ \\__\\____\\_\\  \\ \\__\\ \\__\\\n");
+	usleep(50000);
+	printf("    \\|__|\\|__|\\_________\\|__|\\|__|\n");
+	usleep(50000);
+	printf("             \\|_________|        \n\n");
+	usleep(60000);
+
+	printf("\033[36m");
+	printf("         Holberton Simple Shell\n\n");
+
+	printf("\033[0m");
+}
+
+/**
  * main - the main entry point for the simple shell program
  *
  * Return: 0 on success
@@ -10,9 +43,13 @@ int main(void)
 {
 	char *input = NULL;
 	size_t len = 0;
-	ssize_t = read;
+	ssize_t read;
+	int is_interactive;
 
-	int is_interactive = isatty(STDIN_FILENO);
+	is_interactive = isatty(STDIN_FILENO);
+
+	if (is_interactive)
+		print_banner();
 
 	while (1)
 	{
