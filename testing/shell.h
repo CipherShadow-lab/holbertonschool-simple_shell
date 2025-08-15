26687aa986bcdee4c5597313fe63ec6edd2480f8
cipher <ciphershadow1@gmail.com>
2025-08-15 03:57:19 +0000

    added shell.h

---
+++ testing/shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Extern global */
+extern char **environ;
+
+
+/* function prototypes */
+
+
+/* string helpers */
+
+#endif
