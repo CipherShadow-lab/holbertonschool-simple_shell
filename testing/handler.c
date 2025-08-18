875a11f7ca0662abb34221ed0698bd6d82e559e5
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-18 02:22:21 +0000

    update handler.c

--- testing/handler.c
+++ testing/handler.c
@@ -9,34 +9,31 @@
 
 void handle_command(char *input)
 {
-	char **token_array; /* stores array of tokens parsed from user input */
+	char **args; /* array of tokens/arguments parsed from user input */
 	int builtin_found; /* flag to indicate if command is a builtin */
 
-	token_array = parse_input(input); /* parse user input */
+	args = parse_input(input); /* parse user input */
 
 	/* are there any tokens? (No) */
-	if (token_array == NULL || token_array[0] == NULL)
+	if (args == NULL || args[0] == NULL)
 	{
-		/* TODO: free strings in token array */
-		/* TODO: free token array itself */
+		/* TODO: free args array */
 		return;
 	}
 
 	/* are there any tokens? (Yes) */
 	/* check if built-in command */
-	builtin_found = handle_builtin(token_array);
+	builtin_found = handle_builtin(args[0]);
 
 	/* Is it a built-in command? (Yes) */
 	if (builtin_found != 0)
 	{
-		/* TODO: free strings in token array */
-		/* TODO: free token array itself */
+		/* TODO: free args array */
 		return;
 	}
 	/* Is it a built-in command? (No) */
 	/* Send to execute_command to find and execute command */
-	execute_command(token_array);
+	execute_command(args);
 
-	/* TODO: free strings in token array */
-	/* TODO: free token array itself */
+	/* TODO: free args array */
 }
