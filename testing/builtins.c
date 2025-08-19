e2d49c3637e62839efa55807fc4b9722119da49d
cipher <ciphershadow1@gmail.com>
2025-08-19 06:16:55 +0000

    fixed exit issue with builtins

--- testing/builtins.c
+++ testing/builtins.c
@@ -34,24 +34,22 @@ int handle_builtin(char **input, char *shell_name, int line_number)
 	if (_strcmp(input[0], "exit") == 0)
 	{
 		if (input[1])
-		{
 			fprintf(stderr, "%s: %d: exit: extra operand '%s'\n",
-				shell_name, line_number, input[1]);
-			return (2);
-		}
+					shell_name, line_number, input[1]);
 		exit(0);
+		return (2);
 	}
 	else if (_strcmp(input[0], "env") == 0)
 	{
 		if (input[1])
 		{
-            fprintf(stderr, "%s: %d: env: extra operand '%s'\n",
-                    shell_name, line_number, input[1]);
+			fprintf(stderr, "%s: %d: env: extra operand '%s'\n",
+					shell_name, line_number, input[1]);
 			return (1);
 		}
-        for (int i = 0; environ[i]; i++)
-            printf("%s\n", environ[i]);
-        return (1);
+		for (int i = 0; environ[i]; i++)
+			printf("%s\n", environ[i]);
+		return (1);
 	}
 	else if (_strcmp(input[0], "cd") == 0)
 	{
@@ -59,19 +57,16 @@ int handle_builtin(char **input, char *shell_name, int line_number)
 			path = input[1];
 		else
 			path = getenv("HOME");
-
 		if (chdir(path) != 0)
 			fprintf(stderr, "%s: %d: cd: can't cd to %s\n",
-				shell_name, line_number, path);
+					shell_name, line_number, path);
 		else if (getcwd(cwd, sizeof(cwd)))
 			printf("📁 Moved to: %s\n", cwd);
 		return (1);
 	}
 	else if (_strcmp(input[0], "clear") == 0)
-	{
 		write(STDOUT_FILENO, "\033[H\033[J", 6);
 		return (1);
-	}
+
 	return (0);
 }
-
