7a06f9beb1f1e02761e2f28094087f284003a74d
tranbnn <tranbn@outlook.com>
2025-08-19 13:55:53 +0000

    updating executor

--- testing/executor.c
+++ testing/executor.c
@@ -6,40 +6,45 @@
  * Return: result of the command, or error message
  */
 
-void execute_command(char **input_command)
+int execute_command(char **input_command)
 {
 	pid_t child_pid;
 	char *command_path;
+	int status;
 
 	/* safety checks: empty or invalid inputs */
 	if (input_command == NULL || input_command[0] == NULL)
-		return;
-	/* new shell is created */
-	child_pid = fork();
-	/* in case of fork failure */
-	if (child_pid == -1)
 	{
-		fprintf(stderr, "Fork failed\n");
+		return (-1);
 	}
-	/* if child process creation was successful */
-	else if (child_pid == 0)
+	child_pid = fork(); /* new child process created */
+
+	if (child_pid == -1) /* in case of forking failure */
 	{
-		/* finds full command and stores into command path */
-		command_path = find_in_path(input_command[0]);
-		/* if command cannot be found */
-		if (command_path == NULL)
+		perror("Fork failed");
+		return (-1);
+	}
+	else if (child_pid == 0) /* if forking process success */
+	{
+		command_path = find_in_path(input_command[0]); /* searching for command */
+		if (command_path == NULL) /* if command cannot be found */
 		{
-			fprintf(stderr, "./shell: %s not found\n", input_command[0]);
+			fprintf(stderr, "shell: %s not found\n", input_command[0]);
 			free_args(input_command);
 			exit(EXIT_FAILURE);
 		}
-		/* executes the command */
-		execve(command_path, input_command, environ);
-		/* if execve fails */
-		fprintf(stderr, "execve failed\n");
+		execve(command_path, input_command, environ); /* execute the command */
+		perror("execve failed"); /* if execve fails */
+		free(command_path);
 		free_args(input_command);
 		exit(EXIT_FAILURE);
 	}
 	else
-		wait(NULL);
+	{
+		wait(&status); /* parent will wait for child */
+		if (WIFEXITED(status)) /* if child exit was success, return exit code */
+			return (WEXITSTATUS(status));
+		else /* if child exit was failure, return -1 */
+			return (-1);
+	}
 }
