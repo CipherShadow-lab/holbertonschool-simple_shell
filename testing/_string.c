57b89ff07852b7501777c1a3ef101eb333d0c937
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-19 10:19:36 +0000

    add _strcpy to _string.c

--- testing/_string.c
+++ testing/_string.c
@@ -10,7 +10,7 @@
 
 int _strcmp(const char *s1, const char *s2)
 {
-    int i = 0;
+	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
@@ -39,3 +39,21 @@ int _strlen(const char *s)
 	return (len);
 }
 
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: pointer to string dest
+ * @src: pointer to string src
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' ; i = i + 1)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
