1fa53951f3f8b4a2cf6bcb82be32d7ef5ca50383
cipher <ciphershadow1@gmail.com>
2025-08-20 05:35:54 +0000

    fix: _string.c

--- testing/_string.c
+++ testing/_string.c
@@ -108,29 +108,3 @@ char *_strcat(char *dest, const char *src)
 
 	return (dest);
 }
-
-/**
- * _strdup - creates a duplicate copy of a string
- * @str: string to duplicate
- *
- * Return: pointer to duplicated string, otherwise NULL
- */
-
-char *_strdup(const char *str)
-{
-	char *dup;
-	size_t len;
-
-	if (str == NULL)
-		return (NULL);
-
-	len = _strlen(str);
-	dup = malloc(len + 1);
-
-	if (dup == NULL)
-		return (NULL);
-
-	_strcpy(dup, str);
-
-	return (dup);
-}
