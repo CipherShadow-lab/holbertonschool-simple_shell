c790f188aa01c91d1ecb82ef1a55b92259936cc6
cipher <ciphershadow1@gmail.com>
2025-08-20 04:32:02 +0000

    update executor.c

--- testing/executor.c
+++ testing/executor.c
@@ -1,38 +1,48 @@
 #include "shell.h"
 
-void execute_command(char **args, char *command_path, char *shell_name, int line_number)
+/**
+ * execute_command - executes a command by forking and calling execve
+ * @args: array of arguments
+ * @command_path: full path to the command library
+ * @shell_name: name of shell program
+ * @line_number: line number in session or script
+ *
+ * Return: void
+ */
+
+void execute_command(
+		char **args,
+		char *command_path,
+		char *shell_name,
+		int line_number
+		)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    printf("DEBUG: execute_command: forking to run '%s'\n", command_path);
-    pid = fork();
+	printf("DEBUG: execute_command: forking to run '%s'\n", command_path);
+	pid = fork();
 
-    if (pid == -1)
-    {
-        perror("fork");
-        free(command_path);
-        return;
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+		free(command_path);
+		return;
+	}
 
-    if (pid == 0)
-    {
-        printf("DEBUG: args passed to execve:\n");
-        for (int i = 0; args[i] != NULL; i++)
-            printf("  args[%d]: '%s'\n", i, args[i]);
-        // Child process
-        if (execve(command_path, args, environ) == -1)
-        {
-            perror("execve failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        // Parent process
-        waitpid(pid, &status, 0);
-        printf("DEBUG: Parent process: child finished with status %d\n", status);
-        free(command_path);
-    }
+	if (pid == 0)
+	{
+		for (int i = 0; args[i] != NULL; i++)
+			printf("  args[%d]: '%s'\n", i, args[i]);
+		if (execve(command_path, args, environ) == -1)
+		{
+			perror("execve failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		free(command_path);
+	}
 }
-
