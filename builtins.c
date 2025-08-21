4fc2f49214ded64fef0ccc84416b24a07ac969dd
cipher <ciphershadow1@gmail.com>
2025-08-21 04:37:57 +0000

    updated builtins.c

--- builtins.c
+++ builtins.c
@@ -32,6 +32,14 @@ int handle_builtin(char **input, char *shell_name, int line_number)
 	char *path;
 	int i;
 
+	if (_strcmp(input[0], "exit") == 0)
+	{
+		if (input[1])
+			fprintf(stderr, "%s: %d: exit: extra operand '%s'\n",
+					shell_name, line_number, input[1]);
+		exit(0);
+	}
+
 	if (_strcmp(input[0], "env") == 0)
 	{
 		if (input[1])
