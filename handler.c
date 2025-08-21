a49c37403d98061616cbc7fb504abb784b3f26c7
cipher <ciphershadow1@gmail.com>
2025-08-21 20:44:04 +0000

    included last_status for exit command

--- handler.c
+++ handler.c
@@ -5,50 +5,52 @@
  * @input: user input string
  * @shell_name: name of shell program
  * @line_number: command number
+ * @last_status: last exit status
  *
  * Return: void
  */
 
-void handle_command(char *input, char *shell_name, int line_number)
+void handle_command(char *input, char *shell_name,
+		int line_number, int *last_status)
 {
 	char **args, *full_path;
 	int builtin_found;
-	size_t len, cmd_len;
+	size_t len;
 
 	len = strlen(input);
 	if (len > 0 && input[len - 1] == '\n')
 		input[len - 1] = '\0';
+
 	args = parse_input(input);
 	if (args == NULL || args[0] == NULL)
 	{
-		if (args != NULL)
-			free_args(args);
+		free_args(args);
 		return;
 	}
-	cmd_len = strlen(args[0]);
-	if (cmd_len > 0 && args[0][cmd_len - 1] == '\n')
-		args[0][cmd_len - 1] = '\0';
+
 	builtin_found = handle_builtin(args, shell_name, line_number);
-	if (builtin_found == 2)
-	{
-		free_args(args);
-		exit(0);
-	}
+
+	if (builtin_found == 2 || builtin_found == 3)
+		process_builtin_exit(builtin_found, args, *last_status);
+
 	if (builtin_found == 1)
 	{
+		*last_status = 0;
 		free_args(args);
 		return;
 	}
+
 	full_path = find_in_path(args[0]);
 	if (full_path == NULL)
 	{
 		fprintf(stderr, "%s: %d: %s: not found\n",
 				shell_name, line_number, args[0]);
 
+		*last_status = 127;
 		free_args(args);
-		exit(127);
+		return;
 	}
-	execute_command(args, full_path);
+	*last_status = execute_command(args, full_path);
 	free(full_path);
 	free_args(args);
 }
