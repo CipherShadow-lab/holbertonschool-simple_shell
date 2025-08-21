5b87a7ffa92f2ba771398ad6ebd5bda5a6052426
cipher <ciphershadow1@gmail.com>
2025-08-21 01:28:58 +0000

    update handler.c

--- handler.c
+++ handler.c
@@ -11,13 +11,14 @@
 
 void handle_command(char *input, char *shell_name, int line_number)
 {
-	char **args, *full_path, *path;
+	char **args, *full_path;
 	int builtin_found;
 	size_t len, cmd_len;
 
 	len = strlen(input);
 	if (len > 0 && input[len - 1] == '\n')
 		input[len - 1] = '\0';
+
 	args = parse_input(input);
 	if (args == NULL || args[0] == NULL)
 	{
@@ -35,14 +36,13 @@ void handle_command(char *input, char *shell_name, int line_number)
 		free(args);
 		return;
 	}
+
 	full_path = find_in_path(args[0]);
 	if (full_path == NULL)
 	{
 		fprintf(stderr, "%s: %d: %s: not found\n",
 				shell_name, line_number, args[0]);
-		path = _getenv("PATH");
-		if (path == NULL || *path == '\0')
-			write(STDOUT_FILENO, "OK\n", 3);
+
 		free(args);
 		exit(127);
 	}
